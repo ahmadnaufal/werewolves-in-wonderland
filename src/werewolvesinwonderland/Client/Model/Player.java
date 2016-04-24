87cee8d332ab08048bfde848904c0cc6ec62e00c
Ahmad N. F <fahmadnaufal@gmail.com>
2016-04-24 17:14:29 +0700

    add models for client: Game and Player

---
+++ src/werewolvesinwonderland/Client/Model/Player.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package werewolvesinwonderland.Client.Model;
+
+/**
+ *
+ * @author Ahmad Naufal Farhan
+ */
+public class Player {
+    private int playerId;
+    private String userName;
+    
+    private String role = "";
+    private boolean isAlive = true;
+    
+    public Player(int playerId, String userName) {
+        this.playerId = playerId;
+        this.userName = userName;
+    }
+
+    /**
+     * @return the playerId
+     */
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    /**
+     * @param playerId the playerId to set
+     */
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * @return the isDead
+     */
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    /**
+     * @param isAlive the isAlive to set
+     */
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+    
+    /**
+     * Checking if the player is in the game
+     * @return the player ingame status
+     */
+    public boolean isPlayerInGame() {
+        return !role.equals("") && isAlive;
+    }
+}
