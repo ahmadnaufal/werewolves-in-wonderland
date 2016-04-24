1ee0ab464f366a6eed03d724ad09a779f515e7f5
Tifani Warnita <tiffayumuyuka@gmail.com>
2016-04-24 14:26:15 +0700

    Edit Identification class

--- src/werewolvesinwonderland/Server/Identification.java
+++ src/werewolvesinwonderland/Server/Identification.java
@@ -15,16 +15,35 @@ public class Identification {
      * General method parameter names
      */
     public static final String PRM_METHOD = "method";
-    public static final String PRM_USERNAME = "username";
     public static final String PRM_STATUS = "status";
     public static final String PRM_DESCRIPTION = "description";
     
     /**
      * Game-Specific parameter names
      */
+    // Join Game
+    public static final String PRM_USERNAME = "username";
+    public static final String PRM_PLAYERID = "player_id";
+    // Leave Game
     public static final String PRM_TIME = "time";
     public static final String PRM_DAYS = "days";
     public static final String PRM_ROLE = "role";
+    // List Client
+    public static final String PRM_CLIENTS = "clients";
+    // Clients Object
+    public static final String PRM_ISALIVE = "is_alive";
+    public static final String PRM_ADDR = "address";
+    public static final String PRM_PORT = "port";
+    // Paxos Prepare/Accept Proposal
+    public static final String PRM_PROPOSALID = "proposal_id";
+    public static final String PRM_PREVACC = "previous_accepted";
+    public static final String PRM_KPUID = "kpu_id";
+    // Info Werewolf
+    public static final String PRM_VOTESTATUS = "vote_status";
+    public static final String PRM_PLAYERKILLED = "player_killed";
+    public static final String PRM_VOTERESULT = "vote_result";
+    // Game Over
+    public static final String PRM_WINNER = "winner";
     
     /**
      * Method Parameter possible values
@@ -51,4 +70,21 @@ public class Identification {
     public static final String STATUS_OK = "ok";
     public static final String STATUS_ERROR = "error";
     public static final String STATUS_FAIL = "fail";
+    
+    /**
+     * Description Parameter possible values
+     */
+    public static final String DESC_USEREXISTS = "user exists";
+    public static final String DESC_GAMESTARTED = "game is started";
+    public static final String DESC_GAMERUNNING = "please wait, game is currently running";
+    public static final String DESC_WRONGREQUEST = "wrong request";
+    public static final String DESC_LISTCLIENT = "list of clients retrieved";
+    public static final String DESC_ACCEPTED = "accepted";
+    public static final String DESC_REJECTED = "rejected";
+    
+    /**
+     * Time Parameter possible values
+     */
+    public static final String TIME_DAY = "day";
+    public static final String TIME_NIGHT = "night";
 }
